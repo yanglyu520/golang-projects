adc785d01c82d22ff9529f1089e95194b5bbf37d
yanglyu2021 <thefloweroflife520@gmail.com>
2022-01-07 14:42:23 +1100

    mv repos

---
+++ go-web-app/main.go
@@ -0,0 +1,18 @@
+//package is a way to separate into library
+//single package is easiest to start
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site! </h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", nil)
+}
